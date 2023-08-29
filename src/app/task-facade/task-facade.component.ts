941d49bd943d7322bf0ebd56e87e751e4817ca59
Lucas <a@a.fr>
2023-08-29 08:56:31 +0200

    facade

---
+++ src/app/task-facade/task-facade.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Task } from '../models/Task';
+import { TaskFacadeService } from '../services/task-facade.service';
+
+@Component({
+  selector: 'app-task-facade',
+  templateUrl: './task-facade.component.html',
+  styleUrls: ['./task-facade.component.scss'],
+})
+export class TaskFacadeComponent {
+  tasklist: Task[] = [];
+  constructor(private taskFacadeService: TaskFacadeService) {}
+  ngOnInit() {
+    this.tasklist = this.taskFacadeService.getListTask();
+  }
+}
--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule, formatDate } from '@angular/common';
 import { TaskComponent } from './task.component';
 import { MainTaskComponent } from '../main-task/main-task.component';
 import { FormatDatePipe } from '../format-date.pipe';
 import { RouterModule } from '@angular/router';
 import { CRUDTaskService } from '../services/crudtask.service';
+import { TaskFacadeComponent } from '../task-facade/task-facade.component';
+import { TaskFacadeService } from '../services/task-facade.service';
 
 @NgModule({
-  declarations: [TaskComponent, MainTaskComponent, FormatDatePipe],
+  declarations: [
+    TaskComponent,
+    MainTaskComponent,
+    FormatDatePipe,
+    TaskFacadeComponent,
+  ],
   imports: [
     CommonModule,
-    RouterModule.forChild([{ path: 'display', component: TaskComponent }]),
+    RouterModule.forChild([
+      { path: 'display', component: TaskComponent },
+      { path: 'facade', component: TaskFacadeComponent },
+    ]),
   ],
-  providers: [CRUDTaskService],
+  providers: [CRUDTaskService, TaskFacadeService],
 })
 export class TaskModule {}
