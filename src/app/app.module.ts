7500decedc7872d03e8c44a1d3823f246a41b7ce
Lucas <a@a.fr>
2023-08-29 14:12:28 +0200

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { TaskModule } from './task/task.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AddTaskFormModule } from './add-task-form/add-task-form.module';
 
 @NgModule({
   declarations: [AppComponent, HelloWorldComponent],
-  imports: [BrowserModule, AppRoutingModule, TaskModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TaskModule,
+    HttpClientModule,
+    AddTaskFormModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
